a3669c6bbcd4f8d22e81945e198552d86a3c3e25
gabrielpvicente <gabriel.p.vicente@hotmail.com>
2020-04-16 09:43:23 -0300

    Primeiro Commit

--- URI1160.java
+++ URI1160.java
@@ -3,23 +3,31 @@ import java.util.Scanner;
 public class URI1160{
     public static void main(String args[]){
         Scanner teclado = new Scanner(System.in);
-        int x, y, qtd, a, soma;
-        qtd = teclado.nextInt();
-        a = soma = 0;
-        
-        while (a < qtd){
-            x = teclado.nextInt();
-            y = teclado.nextInt();
-            if (x % 2 == 0){
-                x++;
-            }
-            for (int i = 0; i < y; i++){
-                soma = soma + x;
-                x = x + 2;
+        int t, x, pa, pb, somaa, somab;
+        float g1, g2;
+
+        x = teclado.nextInt();
+        for (int i = 0 ; i < x ; i++){
+            pa = teclado.nextInt();
+            pb = teclado.nextInt();
+            g1 = teclado.nextFloat();
+            g2 = teclado.nextFloat();
+            t=0;
+
+            do {
+                pa = (int) (pa + (pa * g1/100));
+                pb = (int) (pb + (pb * g2/100));
+                t++;
+                if (t>100){
+                    break;
                 }
-            a++;
-            System.out.println(soma);
-            soma = 0;
+            } while (pa <= pb);
+            if (t>100){
+                System.out.println("Mais de 1 seculo.");
+            }
+            else{
+                System.out.println(t+" anos.");
+            }
         }
     }
 }
